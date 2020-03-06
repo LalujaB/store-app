74e097a04d2e440f0e2f7a3d672e470d765704b8
Ime Prezime <moj_email@gmail.com>
2020-03-06 19:45:35 +0100

    Zadatak 1 i 2

---
+++ src/components/AppProducts.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Products.vue</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppProducts',
+        components: {
+
+        }
+    }
+</script>
+
+<style>
+</style>
